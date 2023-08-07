e315faaa8c97aadff001d035452e0aafbbb74e43
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-08-07 13:01:50 +0530

    adjustment*

--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
+router.post("/logout", authController.postLogout);
 module.exports = router;
